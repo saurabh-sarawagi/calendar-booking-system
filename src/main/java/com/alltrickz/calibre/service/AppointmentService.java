063f7222d27879270263e51e1bc7d42c4f36744e
Saurabh Sarawagi <sauru1503@gmail.com>
2025-08-13 16:42:59 +0530

    [4a] Validating using available time slot api when booking appointment

--- src/main/java/com/alltrickz/calibre/service/AppointmentService.java
+++ src/main/java/com/alltrickz/calibre/service/AppointmentService.java
@@ -5,6 +5,7 @@ import com.alltrickz.calibre.dao.AvailabilityRepository;
 import com.alltrickz.calibre.dao.OwnerRepository;
 import com.alltrickz.calibre.dto.AppointmentRequestDTO;
 import com.alltrickz.calibre.dto.AppointmentResponseDTO;
+import com.alltrickz.calibre.dto.TimeSlotResponseDTO;
 import com.alltrickz.calibre.entity.Appointment;
 import com.alltrickz.calibre.entity.AvailabilityRule;
 import com.alltrickz.calibre.entity.Owner;
@@ -14,6 +15,8 @@ import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
     private final AvailabilityRepository availabilityRepository;
     private final OwnerRepository ownerRepository;
+    private final TimeSlotService timeSlotService;
 
     public AppointmentResponseDTO bookAppointment(AppointmentRequestDTO appointmentRequestDTO) throws Exception {
         Owner owner = ownerRepository.findById(appointmentRequestDTO.getOwnerId()).orElseThrow(() -> new Exception("Owner Not Found"));
@@ -36,20 +40,33 @@ public class AppointmentService {
         LocalTime end = LocalTime.parse(appointmentRequestDTO.getEndTime());
 
         if (!end.equals(start.plusHours(1))) {
-            throw new Exception("Only 1 hour slot is allowed for booking");
+            throw new Exception("End Time should always be 1 hour more than Start Time");
         }
 
-        if (start.isBefore(availabilityRule.getStartTime()) || end.isAfter(availabilityRule.getEndTime())) {
-            throw new Exception("Slot not within availability");
-        }
+//        if (start.isBefore(availabilityRule.getStartTime()) || end.isAfter(availabilityRule.getEndTime())) {
+//            throw new Exception("Slot not within availability");
+//        }
+//
+//        boolean appointmentExists = appointmentRepository.existsByOwnerIdAndDateAndStartTimeAndEndTime(appointmentRequestDTO.getOwnerId(), appointmentRequestDTO.getDate(), start, end);
+//        if (appointmentExists) {
+//            throw new Exception("This Timeslot is not available for booking appointment.");
+//        }
 
-        boolean appointmentExists = appointmentRepository.existsByOwnerIdAndDateAndStartTimeAndEndTime(appointmentRequestDTO.getOwnerId(), appointmentRequestDTO.getDate(), start, end);
-        if (appointmentExists) {
+//        Appointment appointment = AppointmentMapper.mapToEntity(appointmentRequestDTO, owner);
+//        Appointment savedAppointment = appointmentRepository.save(appointment);
+//        return AppointmentMapper.mapToResponse(savedAppointment);
+
+        // Directly checking using available time slot api
+        List<TimeSlotResponseDTO> availableTimeSlots = timeSlotService.getAvailableTimeSlots(appointmentRequestDTO.getOwnerId(), appointmentRequestDTO.getDate());
+
+        Optional<TimeSlotResponseDTO> matchingSlot = availableTimeSlots.stream().filter(slot -> slot.getStart().equals(start) && slot.getEnd().equals(end)).findFirst();
+
+        if (matchingSlot.isPresent()) {
+            Appointment appointment = AppointmentMapper.mapToEntity(appointmentRequestDTO, owner);
+            Appointment savedAppointment = appointmentRepository.save(appointment);
+            return AppointmentMapper.mapToResponse(savedAppointment);
+        } else {
             throw new Exception("This Timeslot is not available for booking appointment.");
         }
-
-        Appointment appointment = AppointmentMapper.mapToEntity(appointmentRequestDTO, owner);
-        Appointment savedAppointment = appointmentRepository.save(appointment);
-        return AppointmentMapper.mapToResponse(savedAppointment);
     }
 }
